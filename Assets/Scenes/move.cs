1123db76ee5da2b6d13a2e24d5e46aec3bc0c813
Sinacam <stinkingmadgod@gmail.com>
2018-10-07 23:55:04 +0800

    I hate git
    I really do

---
+++ Assets/Scenes/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 v2 = this.transform.position;
+        v2.y += 0.05f;
+        this.transform.position = v2;
+
+    }
+}
