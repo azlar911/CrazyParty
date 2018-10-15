dfe4f8cd270f66a3a035551964be4e7a20a80a83
Sinacam <stinkingmadgod@gmail.com>
2018-10-15 16:23:16 +0800

    Added Game Loader

---
+++ Assets/Scripts/SomeGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+
+
+public class SomeGame : MonoBehaviour
+{
+
+    public Button theButton;
+
+    void Awake()
+    {
+        Persist.net = (NetworkManager)GameObject.Find("NetworkManager").GetComponent(typeof(NetworkManager));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn1 = theButton.GetComponent<Button>();
+        btn1.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    void TaskOnClick()
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button!");
+        Persist.net.ServerChangeScene("loadingNext");
+    }
+
+    void TaskWithParameters(string message)
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log(message);
+    }
+}
