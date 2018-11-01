bd111e1695c23ffe9f317592bfd24167907ad6dc
KIHOSI <amie12349@gmail.com>
2018-11-01 10:02:31 +0800

    已經做好單人的可樂填滿背景（滑鼠）

---
+++ Assets/Scripts/ShakeCola.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCola : MonoBehaviour {
+    private GameObject colaColor;
+
+	// Use this for initialization
+	void Start () {
+		colaColor = GameObject.Find("ColaColor");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        //print("test");
+        //colaColor.transform.Translate(Vector2.up);
+        colaColor.transform.Translate(new Vector2(0,10));
+    }
+}
