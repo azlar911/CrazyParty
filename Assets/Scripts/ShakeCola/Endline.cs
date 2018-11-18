b7fe1117812cc099e19c65ca47718f07021be3aa
KIHOSI <amie12349@gmail.com>
2018-11-18 21:32:57 +0800

    有加好trigger偵測，但還沒成功將endline結果傳給click

---
+++ Assets/Scripts/ShakeCola/Endline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endline : MonoBehaviour {
+    // Use this for initialization
+    GameObject shakeCola;
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Awake()
+    {
+        shakeCola = GameObject.Find("Click"); 
+    }
+
+    //可樂搖滿房間
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("success!");
+        shakeCola.GetComponent<ShakeCola>().getEnd();
+    }
+}
--- Assets/Scripts/ShakeCola/ShakeCola.cs
+++ Assets/Scripts/ShakeCola/ShakeCola.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 public class ShakeCola : PlayerBehaviour {
     private GameObject colaColor;
     private Vector2 yposition = new Vector2(0, 0);
+    private bool playerFinish = false; //玩家是否要結束
 	// Use this for initialization
 	void Start () {
 		colaColor = GameObject.Find("ColaColor");
@@ -22,12 +23,19 @@ public class ShakeCola : PlayerBehaviour {
             CmdShakeCola();
         }
 
-        print(colaColor.transform.position.y);
+        if(playerFinish){ //如果玩家已經結束遊戲
+            LevelDone();
+        }
 	}
 
     [Command] //執行shake cola動作（要從client傳到server)
     void CmdShakeCola()
     {
         colaColor.transform.Translate(yposition + new Vector2(0,10));
+    }
+
+    public void getEnd(){ //Endline如果偵測到可樂已經搖滿房間，會呼叫這個function，設定該玩家已可結束遊戲
+        print("get end!");
+        playerFinish = true;
     }
 }
