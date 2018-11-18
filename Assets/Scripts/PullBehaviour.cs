68621c19871833d6cfe0c89de56400fb9b964c2b
Sinacam <stinkingmadgod@gmail.com>
2018-11-18 18:49:41 +0800

    Removed debug

--- Assets/Scripts/PullBehaviour.cs
+++ Assets/Scripts/PullBehaviour.cs
@@ -7,14 +7,12 @@ public class PullBehaviour : PlayerBehaviour
 {
     float dir
     {
-        get { Debug.Log("dir read as " + (role % 2 == 0 ? 1 : -1).ToString()); return role % 2 == 0 ? 1 : -1; }
+        get { return role % 2 == 0 ? 1 : -1; }
     }
 
     void Start()
     {
         transform.position += new Vector3(dir * 7, 0, 0);
-        Debug.Log("role = " + role.ToString());
-        Debug.Log("dir = " + dir.ToString());
     }
 
     void Update()
