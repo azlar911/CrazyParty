fe374f6ada9a2b786ab7536ddb264b3c9a29d38c
nicole011635 <nicole2092455@gmail.com>
2018-11-19 16:55:15 +0800

    add levelend add begining animation

--- Assets/Scripts/PullBehaviour.cs
+++ Assets/Scripts/PullBehaviour.cs
@@ -15,15 +15,52 @@ public class PullBehaviour : PlayerBehaviour
         transform.position += new Vector3(dir * 7, 0, 0);
     }
 
+    float Timer;
+
     void Update()
     {
-        if (!isLocalPlayer)
+        if (!isLocalPlayer) {
             return;
-    }
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+            Debug.Log(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.gameObject.name == "Mole(Clone)");
+                    if (hit.collider.gameObject.tag == "rightPull" && dir == 1)
+                    {
+                        Cmdmove();
+                    }
+                    else if(hit.collider.gameObject.tag == "leftPull" && dir == -1)
+                    {
+                        Cmdmove();
+                    }
+                }
+            }*/
+        }
+
+        
+        Timer += Time.deltaTime;
+        print("yee");
+        print(Timer);
+
+        if (Timer > 10)
+        {
+            LevelDone();
+        }
+        
+    }
     void OnMouseDown()
     {
-        GameObject.Find("rope").transform.position += new Vector3(dir, 0, 0);
+        GameObject.Find("rope").transform.position += new Vector3(dir/2, 0, 0);
     }
-
+    /*
+    [Command]
+    void Cmdmove()
+    {
+        GameObject.Find("rope").transform.position += new Vector3(dir, 0, 0);
+    }*/
 }
