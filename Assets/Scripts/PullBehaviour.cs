81c3138c676126fda83ab171d47bf4f537f9f550
Sinacam <stinkingmadgod@gmail.com>
2018-11-19 22:17:58 +0800

    Fixed non local for tug of war

--- Assets/Scripts/PullBehaviour.cs
+++ Assets/Scripts/PullBehaviour.cs
@@ -15,52 +15,24 @@ public class PullBehaviour : PlayerBehaviour
         transform.position += new Vector3(dir * 7, 0, 0);
     }
 
-    float Timer;
+    float elapsed;
 
     void Update()
     {
-        if (!isLocalPlayer) {
+        if (!isLocalPlayer)
             return;
-            /*
-            if (Input.GetMouseButtonDown(0))
-            {
-            Debug.Log(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
-                if (hit.collider != null)
-                {
-                    Debug.Log(hit.collider.gameObject.name == "Mole(Clone)");
-                    if (hit.collider.gameObject.tag == "rightPull" && dir == 1)
-                    {
-                        Cmdmove();
-                    }
-                    else if(hit.collider.gameObject.tag == "leftPull" && dir == -1)
-                    {
-                        Cmdmove();
-                    }
-                }
-            }*/
-        }
+        elapsed += Time.deltaTime;
 
-        
-        Timer += Time.deltaTime;
-        print("yee");
-        print(Timer);
-
-        if (Timer > 10)
-        {
+        if (elapsed > 10)
             LevelDone();
-        }
-        
+
     }
     void OnMouseDown()
     {
-        GameObject.Find("rope").transform.position += new Vector3(dir/2, 0, 0);
+        if (!isLocalPlayer)
+            return;
+
+        GameObject.Find("rope").transform.position += new Vector3(dir / 2, 0, 0);
     }
-    /*
-    [Command]
-    void Cmdmove()
-    {
-        GameObject.Find("rope").transform.position += new Vector3(dir, 0, 0);
-    }*/
 }
