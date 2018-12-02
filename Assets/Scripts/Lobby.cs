9e0775350a8e4e1dc4178026166e11baf1ab770b
Sinacam <stinkingmadgod@gmail.com>
2018-12-02 11:43:32 +0800

    Fixed PC compatibility and bugs

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -6,9 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class Lobby : NetworkBehaviour
 {
+    void Start()
+    {
+        Persist.goodScores = new SyncListInt();
+        Persist.evilScores = new SyncListInt();
+    }
+
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetTouch(0).phase == TouchPhase.Began) && Persist.net.IsClientConnected())
+        if ((Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)) && Persist.net.IsClientConnected())
             Persist.net.ServerChangeScene("LoadingNext");
     }
 }
