a3e451d7b433c8a9e5d0e2e0d8e47b072bc86c90
KIHOSI <amie12349@gmail.com>
2018-12-02 19:51:42 +0800

    add endline back

---
+++ Assets/Scripts/ShakeCola/Endline.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endline : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //可樂搖滿房間
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Step one
+        ShakeCola[] clones = FindObjectsOfType<ShakeCola>();
+
+        // Step two
+        foreach (var shakeCola in clones)
+            shakeCola.getEnd();
+        //print("success!");
+
+    }
+}
