c7e6848e992a352c508378ae45e4da8faca5eca7
Sinacam <stinkingmadgod@gmail.com>
2018-12-04 00:11:30 +0800

    Game logic

---
+++ Assets/Scripts/Balance/BallController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : PlayerBehaviour
+{
+    public float sensitivity = 10f;
+    public float windStrength = 10f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = (Rigidbody)gameObject.GetComponent(typeof(Rigidbody));
+    }
+    
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * sensitivity;
+        var y = Input.GetAxis("Vertical") * sensitivity;
+
+        var winds = GameObject.FindGameObjectsWithTag("wind");
+        foreach (var w in winds)
+        {
+            var wc = (WindController)w.GetComponent(typeof(WindController));
+            var a = wc.GetAzimuth() * Mathf.Deg2Rad;
+            x += Mathf.Cos(a) * windStrength;
+            y += Mathf.Sin(a) * windStrength;
+        }
+
+        rb.AddForce(new Vector3(x * Time.deltaTime, y * Time.deltaTime, 0));
+
+        if(transform.position.sqrMagnitude > 16)
+        {
+            LevelDone(0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Balance/WindController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WindController : PlayerBehaviour
+{
+    public float sensitivity = 10, cutoff = 2;
+
+    public float GetAzimuth()
+    {
+        var deg = Vector3.Angle(Vector3.right, transform.position);
+        if (transform.position.y < 0)
+            return 360 - deg;
+        else
+            return deg;
+    }
+
+    void AddAzimuth(float deg)
+    {
+        CmdAddAzimuth(deg);
+    }
+
+    [Command]
+    void CmdAddAzimuth(float deg)
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, deg);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        var a = GetAzimuth();
+        float da = 0;
+
+        // Cutoff to avoid ambiguoous directions.
+        if (a < 180 - cutoff && a > cutoff)
+            da -= x;
+        else if(a > 180 + cutoff && a < 360 - cutoff)
+            da += x;
+
+        if (a < 90 - cutoff || a > 270 + cutoff)
+            da += y;
+        else if(a > 90 + cutoff && a < 270 - cutoff)
+            da -= y;
+
+        da = Mathf.Clamp(da, -1f, 1f) * Time.deltaTime * sensitivity;
+
+        Debug.Log("a = " + GetAzimuth() + ", da = " + da);
+
+        AddAzimuth(da);
+    }
+}
